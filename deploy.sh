f0b2a5a41ac1505a856dd1fb577dd6cff3e73016
Wan Wan <simon@macintosh-2.attlocal.net>
2019-06-13 23:53:30 -0700

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t wanwanzainaer/multi-client:latest -t wanwanzainaer/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t wanwanzainaer/multi-server:latest -t wanwanzainaer/multi-client:$SHA -f ./server/Dockerfile ./server
+docker build -t wanwanzainaer/multi-worker:latest -t wanwanzainaer/multi-client:$SHA -f ./worker/Dockerfile ./worker
+docker push wanwanzainaer/multi-client:latest
+docker push wanwanzainaer/multi-server:latest
+docker push wanwanzainaer/multi-worker:latest
+
+docker push wanwanzainaer/multi-client:$SHA
+docker push wanwanzainaer/multi-server:$SHA
+docker push wanwanzainaer/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=wanwanzainaer/multi-server:$SHA
+kubectl set image deployments/client-deployment client=wanwanzainaer/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=wanwanzainaer/multi-worker:$SHA
